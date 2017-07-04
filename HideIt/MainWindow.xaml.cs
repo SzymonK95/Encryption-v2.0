adb5d77c8a16417ca0da09a185a5e68dfcdc5ee1
Szymon <szymon_kaszuba97@wp.pl>
2017-07-04 21:32:13 +0200

    UI cd U can set Image

--- HideIt/MainWindow.xaml.cs
+++ HideIt/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,10 +27,45 @@ namespace HideIt
             InitializeComponent();
         }
 
+        private void buttonEncrypt_MouseEnter(object sender, MouseEventArgs e)
+        {
+            textBlockInfo.Text = "Kliknij by rozpocząć szyfrowanie";
+        }
+
+        private void buttonEncrypt_MouseLeave(object sender, MouseEventArgs e)
+        {
+            textBlockInfo.Text = "";
+        }
+
+        private void buttonDecryption_MouseEnter(object sender, MouseEventArgs e)
+        {
+            textBlockInfo.Text = "Kliknij by rozpocząć deszyfrowanie";
+        }
+
+        private void buttonDecryption_MouseLeave(object sender, MouseEventArgs e)
+        {
+            textBlockInfo.Text = "";
+        }
+
+        private void ButtonBasePicture_MouseEnter(object sender, MouseEventArgs e)
+        {
+            textBlockInfo.Text = "Kliknij by wczytać obraz, który będzie bazą do ukrycia wiadomości";
+        }
+
+        private void ButtonBasePicture_MouseLeave(object sender, MouseEventArgs e)
+        {
+            textBlockInfo.Text = "";
+        }
 
         private void ButtonBasePicture_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPG images (*.jpg)|*.jpg";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ImageBase.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
         }
     }
 }
