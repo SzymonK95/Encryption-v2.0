0e256ea9c8f3d097661a7b3cd46da8b486766e7b
SzymonK95 <szymon_kaszuba97@wp.pl>
2017-07-07 07:21:50 +0200

    untracked items

---
+++ HideIt/Model/Caesar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HideIt.Repository;
+using System.Windows.Controls;
+
+namespace HideIt.Model
+{
+    public class Caesar : EncryptType
+    {
+        private Int32 Key;
+
+        public Caesar(Int32 Key)
+        {
+            this.Key = Key;
+        }
+
+        public override String EncryptAlgorithm(String message)
+        {
+            String enctyptMessage = null;
+
+            foreach (char c in message)
+            {
+                //licznik sie przekraci
+                var elem = (int)c + Key;
+
+                enctyptMessage += (char)elem;
+            }     
+
+            return enctyptMessage; 
+        }
+
+        public override String DecryptAlgorithm(String message)
+        {
+            String decryptMessage = null;
+
+            return decryptMessage;
+        }
+    }
+}
