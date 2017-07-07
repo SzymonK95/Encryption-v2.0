eab546875cc69ec23fd95dcceff1addc4cb1a88c
SzymonK95 <szymon_kaszuba97@wp.pl>
2017-07-07 07:21:02 +0200

    encryptobject

---
+++ HideItTests/UnitTestConsole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HideIt.Model;
+
+namespace HideItTests
+{
+    [TestClass]
+    public class UnitTestConsole
+    {
+        [TestMethod]
+        public void TestMethod_CeasarWithKey_Add5()
+        {
+            EncryptType c = new Caesar(5);
+            Assert.AreEqual(c.EncryptAlgorithm("AAA"), "FFF");
+        }
+
+        [TestMethod]
+        public void TestMethod_CeasarWithKey_Sub5()
+        {
+            EncryptType c = new Caesar(-5);
+            Assert.AreEqual(c.EncryptAlgorithm("???"), ":::");
+        }
+    }
+}
